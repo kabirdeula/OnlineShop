9e871565d7a1fbefa46db4d405e5ea1d8239762c
Kabir Deula <kabirdeula167@gmail.com>
2025-01-17 15:12:01 +0545

    Add use case injection and implement ProductUsecase for product fetching

--- lib/core/dependency_injection/dependency_injection.dart
+++ lib/core/dependency_injection/dependency_injection.dart
@@ -2,11 +2,13 @@ import 'package:get_it/get_it.dart';
 
 import 'repository_injection.dart';
 import 'service_injection.dart';
+import 'usecase_injection.dart';
 
 /// A centralized method to set up dependency injection for the entire application.
 void setupDependencyInjection() {
   setupServiceInjection();
   setupRepositoryInjection();
+  setupUsecaseInjection();
 }
 
 /// The singleton instance of [GetIt] used for dependency injection.
--- lib/features/product/product.dart
+++ lib/features/product/product.dart
@@ -2,4 +2,5 @@ export './data/models/product.dart';
 export './data/repositories/product_repository.dart';
 export './data/sources/local/product_service.dart';
 export './domain/repositories/product_repository.dart';
+export './domain/usecases/product_usecase.dart';
 export './presentation/widgets/product_grid_view.dart';
