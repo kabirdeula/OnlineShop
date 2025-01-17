80c15395320de005e0605eb40740aa4dd7ae0204
Kabir Deula <kabirdeula167@gmail.com>
2025-01-17 21:52:27 +0545

    Add ProductCard widget and integrate it into ProductGridView for improved product display

---
+++ lib/features/product/presentation/widgets/product_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:online_shop/core/constants/constants.dart';
+
+class ProductCard extends StatelessWidget {
+  final Color color;
+  final String image;
+  final String title;
+  final double price;
+
+  const ProductCard({
+    super.key,
+    required this.color,
+    required this.image,
+    required this.title,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: EdgeInsets.all(AppDimensions.padding),
+          height: 175,
+          width: 160,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Image.asset(image),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: AppDimensions.padding / 4,
+          ),
+          child: Text(title, style: TextStyle(color: AppColors.textLight)),
+        ),
+        Text("\$ $price", style: TextStyle(fontWeight: FontWeight.bold)),
+      ],
+    );
+  }
+}
