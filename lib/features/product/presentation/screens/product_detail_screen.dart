6fdce0ab7b43b792e02e4fa6cf8eb5a6a6dcd261
Kabir Deula <kabirdeula167@gmail.com>
2025-01-19 14:48:30 +0545

    Add color selection feature to ProductDetailScreen; update UI to display color options and handle selection

--- lib/features/product/presentation/screens/product_detail_screen.dart
+++ lib/features/product/presentation/screens/product_detail_screen.dart
@@ -31,8 +31,14 @@ class ProductDetailScreen extends StatelessWidget {
           return const Center(child: Text("Product not found"));
         }
 
+        final productColors = [
+          Color(0xFF356C95),
+          Color(0xFFF8C078),
+          Color(0xFFA29B9B),
+        ];
+
         return Scaffold(
-          backgroundColor: hexToColor(product.color),
+          backgroundColor: state.selectedColor ?? hexToColor(product.color),
           appBar: CustomAppBar(
             showBackButton: true,
             actions: [
@@ -55,7 +61,11 @@ class ProductDetailScreen extends StatelessWidget {
                     children: [
                       Container(
                         margin: EdgeInsets.only(top: size.height * 0.3),
-                        height: 500,
+                        padding: EdgeInsets.only(
+                          top: size.height * 0.12,
+                          left: AppDimensions.padding,
+                          right: AppDimensions.padding,
+                        ),
                         decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.only(
@@ -63,8 +73,136 @@ class ProductDetailScreen extends StatelessWidget {
                             topRight: Radius.circular(24),
                           ),
                         ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text("Color"),
+                                      Row(
+                                        children: productColors.map((color) {
+                                          final isSelected =
+                                              state.selectedColor == color;
+                                          return GestureDetector(
+                                            onTap: () => context
+                                                .read<ProductCubit>()
+                                                .changeSelectedColor(color),
+                                            child: Container(
+                                              height: 26,
+                                              width: 26,
+                                              margin: EdgeInsets.only(
+                                                top: AppDimensions.padding / 4,
+                                                right:
+                                                    AppDimensions.padding / 2,
+                                              ),
+                                              padding:
+                                                  const EdgeInsets.all(2.5),
+                                              decoration: BoxDecoration(
+                                                shape: BoxShape.circle,
+                                                border: Border.all(
+                                                  color: isSelected
+                                                      ? color
+                                                      : Colors.transparent,
+                                                  width: 1,
+                                                ),
+                                              ),
+                                              child: DecoratedBox(
+                                                decoration: BoxDecoration(
+                                                  color: color,
+                                                  shape: BoxShape.circle,
+                                                ),
+                                              ),
+                                            ),
+                                          );
+                                        }).toList(),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  child: RichText(
+                                    text: TextSpan(
+                                      style: TextStyle(color: AppColors.text),
+                                      children: [
+                                        TextSpan(text: "Size\n"),
+                                        TextSpan(
+                                          text: "${product.size} cm",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headlineSmall!
+                                              .copyWith(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: AppDimensions.padding),
+                              child: Text(
+                                product.description,
+                                style: TextStyle(height: 1.5),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      Padding(padding: EdgeInsets.all(AppDimensions.padding))
+                      Padding(
+                        padding: EdgeInsets.all(AppDimensions.padding),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Aristocratic Hand Bag",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Text(
+                              product.title,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineSmall!
+                                  .copyWith(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                            ),
+                            SizedBox(height: AppDimensions.padding),
+                            Row(
+                              children: [
+                                RichText(
+                                  text: TextSpan(
+                                    children: [
+                                      TextSpan(text: "Price\n"),
+                                      TextSpan(
+                                        text: "\$${product.price}",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headlineSmall!
+                                            .copyWith(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: AppDimensions.padding,
+                                ),
+                                Expanded(child: Image.asset(product.image)),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
                     ],
                   ),
                 ),
