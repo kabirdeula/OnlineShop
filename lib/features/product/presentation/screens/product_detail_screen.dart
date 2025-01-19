e2bbea471a0356859d85fea9133895c842996242
Kabir Deula <kabirdeula167@gmail.com>
2025-01-19 12:49:18 +0545

    Refactor ProductDetailScreen and ProductGridView to improve product loading and navigation; update state handling and UI elements

--- lib/features/product/presentation/screens/product_detail_screen.dart
+++ lib/features/product/presentation/screens/product_detail_screen.dart
@@ -1,18 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:online_shop/core/constants/constants.dart';
 import 'package:online_shop/core/utils/utils.dart';
 import 'package:online_shop/core/widgets/widgets.dart';
 import 'package:online_shop/features/product/product.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  final Product product;
+  final String productID;
 
-  const ProductDetailScreen({super.key, required this.product});
+  const ProductDetailScreen({super.key, required this.productID});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: hexToColor(product.color),
-      appBar: CustomAppBar(showBackButton: true,),
+    final size = MediaQuery.of(context).size;
+
+    context
+        .read<ProductCubit>()
+        .loadProductById(productId: int.parse(productID));
+
+    return BlocBuilder<ProductCubit, ProductState>(
+      builder: (context, state) {
+        if (state.isLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        final product = state.selectedProduct;
+
+        if (product == null) {
+          return const Center(child: Text("Product not found"));
+        }
+
+        return Scaffold(
+          backgroundColor: hexToColor(product.color),
+          appBar: CustomAppBar(
+            showBackButton: true,
+            actions: [
+              IconButton(
+                icon: Icon(AppIcons.search, color: AppColors.text),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(AppIcons.cart, color: AppColors.text),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: size.height,
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: size.height * 0.3),
+                        height: 500,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(24),
+                            topRight: Radius.circular(24),
+                          ),
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.all(AppDimensions.padding))
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/features/product/presentation/widgets/product_grid_view.dart
+++ lib/features/product/presentation/widgets/product_grid_view.dart
@@ -6,6 +6,7 @@ import 'package:online_shop/features/product/presentation/widgets/product_card.d
 import 'package:online_shop/features/product/product.dart';
 import 'package:online_shop/routes/routes.dart';
 
+/// A widget that displays a grid of products fetched from the state.
 class ProductGridView extends StatelessWidget {
   const ProductGridView({super.key});
 
@@ -14,40 +15,53 @@ class ProductGridView extends StatelessWidget {
     return BlocBuilder<ProductCubit, ProductState>(
       builder: (context, state) {
         if (state.isLoading) {
-          return Center(child: const CircularProgressIndicator());
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         }
-        if (state.products.isEmpty) {
-          return const Center(child: Text("No products found"));
+
+        final products = state.productList;
+        if (products == null || products.isEmpty) {
+          return const Center(
+            child: Text("No products available"),
+          );
         }
+
         return Padding(
           padding: const EdgeInsets.only(bottom: AppDimensions.padding),
-          child: GridView.builder(
-            shrinkWrap: true,
-            physics: const BouncingScrollPhysics(),
-            itemCount: state.products.length,
-            itemBuilder: (context, index) {
-              final product = state.products[index];
-
-              return ProductCard(
-                color: hexToColor(product.color),
-                image: product.image,
-                price: product.price,
-                title: product.title,
-                onTap: () => context.push(
-                  AppRoutes.productDetail.path,
-                  extra: product,
-                ),
-              );
-            },
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: AppDimensions.padding,
-              crossAxisSpacing: AppDimensions.padding,
-              childAspectRatio: 0.72,
-            ),
-          ),
+          child: _buildProductGrid(products),
         );
       },
     );
+  }
+
+  /// Builds the grid view for displaying products.
+  Widget _buildProductGrid(List<Product> products) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const BouncingScrollPhysics(),
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        final product = products[index];
+        return ProductCard(
+          color: hexToColor(product.color),
+          image: product.image,
+          price: product.price,
+          title: product.title,
+          onTap: () => _navigateToProductDetail(context, product.id),
+        );
+      },
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: AppDimensions.padding,
+        crossAxisSpacing: AppDimensions.padding,
+        childAspectRatio: 0.72,
+      ),
+    );
+  }
+
+  /// Navigates to the product detail screen for a specific product ID.
+  void _navigateToProductDetail(BuildContext context, int productId) {
+    context.push("${AppRoutes.productDetail.path}/$productId");
   }
 }
